a809a68a20c4ac4c0036b90f20965fa3931a517c
Parv <mrparveznoor@gmail.com>
2023-03-13 06:24:19 +0000

    secure

---
+++ src/apis/auth/reducer.ts
@@ -0,0 +1,14 @@
+// import { AuthAction, AuthState } from "./types";
+
+// export const authReducer = (state: AuthState, action: AuthAction): AuthState => {
+//   switch (action.type) {
+//     case "SET_ACCESS":
+//       return { ...state, access: action.payload };
+//     case "SET_USER":
+//       return { ...state, user: action.payload };
+//     case "LOGOUT":
+//       return { access: undefined, user: undefined };
+//     default:
+//       return state;
+//   }
+// };
